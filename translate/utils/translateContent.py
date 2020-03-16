3ae749e43435034ba28ebf4e64f055fca3f92adc
a69186005 <315842672@qq.com>
2020-03-17 09:57:02 +1100

    fix bugs

---
+++ translate/utils/translateContent.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+'''
+# Created on Mar-17-20 09:22
+# translateContent.py
+# @author: Lucius
+'''
+import os, django, sys
+sys.path.append(os.path.dirname(os.path.abspath('.')))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'translate.settings'
+django.setup()
+import requests
+import json
+
+from spiderAndTranslate.cd import UserProfile, OriginalBooks, OriginalBooksContent, TanslationBookContentEN, TranslationBooksEN
+from spiderAndTranslate.utils.baiduTranslate import BaiduTranslation
+
+books_en = TranslationBooksEN.objects.all()
+
+for book_en in books_en:
+  book_content = OriginalBooksContent.objects.filter(book = book_en.book)
+  if book_content:
+    for content in book_content:
+      book_content_en = TanslationBookContentEN()
+      book_content_en.book = book_en
+      book_content_en.chapter_name = BaiduTranslation(content.chapter_name, 'zh', 'en').translate()
+      print(BaiduTranslation(content.chapter_name, 'zh', 'en').translate())
+      book_content_en.chapter_index = content.chapter_index
+      book_content_en.chapter_content = BaiduTranslation(content.chapter_content, 'zh', 'en').translate()
+      book_content_en.words = content.words
+      book_content_en.save()
