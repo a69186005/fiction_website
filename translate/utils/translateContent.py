cb79022afba0a3aa5af376ff66c21262b3c2cccf
a69186005 <315842672@qq.com>
2020-04-06 10:57:54 +1000

    update

--- translate/utils/translateContent.py
+++ translate/utils/translateContent.py
@@ -14,21 +14,26 @@ import json
 from spiderAndTranslate.models import UserProfile, OriginalBooks, OriginalBooksContent, TanslationBookContentEN, TranslationBooksEN
 from spiderAndTranslate.utils.baiduTranslate import BaiduTranslation
 
+
 books_en = TranslationBooksEN.objects.all()
 
 for book_en in books_en:
   book_content = OriginalBooksContent.objects.filter(book = book_en.book)
-  if book_content:
-    try:
-      for content in book_content:
-        book_content_en = TanslationBookContentEN()
-        book_content_en.book = book_en
-        book_content_en.chapter_name = BaiduTranslation(content.chapter_name, 'zh', 'en').translate()
-        print(BaiduTranslation(content.chapter_name, 'zh', 'en').translate())
-        book_content_en.chapter_index = content.chapter_index
-        book_content_en.chapter_content = BaiduTranslation(content.chapter_content, 'zh', 'en').translate()
-        book_content_en.words = content.words
-        book_content_en.save()
-    except:
-      print("book_id:" + book_en + ";" + "book_index:" + book_content_en.chapter_index)
-      print("save error")
+  existed_book = TanslationBookContentEN.objects.filter(book = book_en.book)
+  if existed_book:
+    print ("It is an existed book")
+  else:
+    if book_content:
+      try:
+        for content in book_content:
+          book_content_en = TanslationBookContentEN()
+          book_content_en.book = book_en
+          book_content_en.chapter_name = BaiduTranslation(content.chapter_name, 'zh', 'en').translate()
+          print(BaiduTranslation(content.chapter_name, 'zh', 'en').translate())
+          book_content_en.chapter_index = content.chapter_index
+          book_content_en.chapter_content = BaiduTranslation(content.chapter_content, 'zh', 'en').translate()
+          book_content_en.words = content.words
+          book_content_en.save()
+      except:
+        print("book_id:" + book_en + ";" + "book_index:" + book_content_en.chapter_index)
+        print("save error")
