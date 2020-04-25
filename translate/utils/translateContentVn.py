11c8be1cb2489fb8b62f659bcea6e8b0c9c275b3
a69186005 <315842672@qq.com>
2020-04-25 23:38:41 +1000

    update

---
+++ translate/utils/translateContentVn.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+'''
+# Created on Mar-17-20 09:22
+# translateContent.py
+# @author: Lucius
+'''
+import os, django, sys
+sys.path.append(os.path.dirname(os.path.abspath('.')))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'translate.settings'
+django.setup()
+import requests
+import json
+
+from spiderAndTranslate.models import UserProfile, OriginalBooks, OriginalBooksContent, TanslationBookContentVI, TranslationBooksVI
+from spiderAndTranslate.utils.baiduTranslateVn import BaiduTranslation
+
+
+books_vi = TranslationBooksEN.objects.all()
+
+for book_vi in books_vi:
+  book_content = OriginalBooksContent.objects.filter(book = book_en.book)
+  existed_book = TanslationBookContentVI.objects.filter(book = book_en)
+  if existed_book:
+    print ("It is an existed book")
+  else:
+    if book_content:
+      try:
+        for content in book_content:
+          book_content_vi = TanslationBookContentVI()
+          book_content_vi.book = book_vi
+          book_content_vi.chapter_name = BaiduTranslation(content.chapter_name, 'zh', 'en').translate()
+          print(BaiduTranslation(content.chapter_name, 'zh', 'en').translate())
+          book_content_vi.chapter_index = content.chapter_index
+          book_content_vi.chapter_content = BaiduTranslation(content.chapter_content, 'zh', 'en').translate()
+          book_content_vi.words = content.words
+          book_content_vi.save()
+      except:
+        print("book_id:" + book_vi.book_name + ";" + "book_index:" + book_content_vi.chapter_index)
+        print("save error")
